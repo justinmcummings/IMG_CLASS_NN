5fdc0b885f201f9842cceac38e43a93c9f088a72
Justin Cummings <justinmcummings@gmail.com>
2015-12-08 12:13:52 -0600

    commit 5
    deleted duplicated files

--- MatlabCode/sigmoid.m
+++
@@ -1,6 +0,0 @@
-%% Help Function
-% z is calculated in the feed-forward step
-function f_sigmoid = sigmoid(z)
-    %f_sigmoid = 1./(1+exp(-z)); % 0 1 function
-    f_sigmoid = (exp(z)-exp(-z))./(exp(z)+exp(-z)); % -1 1 function
-end
